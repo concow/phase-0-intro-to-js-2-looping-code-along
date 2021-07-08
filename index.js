f7e810cde9e637a23e09fad1d98d7d7b1bdc5a3c
Connor Cowan <ccowan003@comcast.net>
2021-07-07 20:34:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, eventName) {
+    let cards = []
+    for (let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+}
+
+function countDown(countdown) {
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown -= 1
+    }
+    console.log(countdown);
+}
